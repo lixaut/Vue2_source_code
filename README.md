8a137d98c4136ab621fa9cb96e10b47a1437fb56
wangli <wanglixaut@qq.com>
2022-09-03 10:21:39 +0800

    基本配置

---
+++ README.md
@@ -0,0 +1,19 @@
+## 环境搭建
+
+1. 安装依赖
+
+```js
+npm install rollup rollup-plugin-babel @babel/core @babel/preset-env --save-dev
+```
+
+2. 添加配置文件`rollup.confing.js`
+
+3. `package.json`添加配置描述
+
+```js
+    "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "dev": "rollup -cw"
+    },
+```
+
---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+
+// rollup默认可以导出一个对象，作为打包的配置文件
+import babel from 'rollup-plugin-babel'
+export default {
+    // 入口文件
+    input: './src/index.js',
+    // 出口
+    output: {
+        file: './dist/vue.js',
+        name: 'Vue',
+        format: 'umd',
+        // 可以调试源代码
+        sourcemap: true
+    },
+    plugins: [
+        babel({
+            // 排除node_modules所有文件
+            exclude: 'node_modules/**'
+        })
+    ]
+}
